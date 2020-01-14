1bb93d479f3b0c1496a4668b2e0e3aa7caed8e9c
edgardleal <edgardleal@gmail.com>
2020-01-14 13:13:17 -0300

    :star: implements recursive retry

--- bff/src/index.ts
+++ bff/src/index.ts
@@ -17,35 +17,35 @@ server.use(morgan('dev'));
 
 const PORT = process.env.PORT || 3000;
 
-server.get('/users', (req, res) => {
-  const instance = axios.create({
-    baseURL: 'http://users:3000',
-    timeout: 800,
-  });
-  instance.get('/')
+const instance = axios.create({
+  baseURL: 'http://users:3000',
+  timeout: 800,
+});
+
+
+const tryRequest = (req, res, tries = 0) => {
+  return instance
+    .get('/')
     .then(response => response.data)
     .then(res.json.bind(res))
     .catch((error: AxiosError) => {
-
-      const retry = () => {
-        return instance.get('/').then(response => response.data);
-      };
-      console.error(error.message);
-
-      retry().then(res.json.bind(res)).catch(e => {
-        retry().then(res.json.bind(res)).catch(error => {
-          res.status((error.response || { status: 500 }).status);
-          console.error(error.message);
-          res.end(error.message);
-        });
-      });
+      if (tries < 20) {
+        tryRequest(req, res, tries + 1);
+      } else {
+        console.error(error.message);
+        res.status((error.response || { status: 500 }).status);
+        res.end(error.message);
+      }
     });
+};
+
+server.get('/users', (req, res) => {
+  tryRequest(req, res);
 });
 
-server.listen(PORT, (err) => {
+server.listen(PORT, err => {
   if (err) {
     console.error(err);
   }
   console.log(`Listeining on port ${PORT}`);
 });
-
